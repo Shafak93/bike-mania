5095dbfa3432d20756596e11754cf9e28420ca6d
Shafak93 <shafakctg64@gmail.com>
2022-05-08 13:42:12 +0600

    require auth functionality added

---
+++ src/Components/Authentication/RequireAuth/RequireAuth.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation } from 'react-router-dom';
+import auth from '../../../firebase.init';
+
+const RequireAuth = ({children}) => {
+    const [user, loading] = useAuthState(auth);
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
+    const location = useLocation();
+    if(loading){
+        return <p>Loading...</p>
+    }
+
+    if(!user){
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+
+    if(user.providerData[0]?.providerId === 'password' && !user.emailVerified){
+        return <div>
+             <h3 className='text-danger'>Your email is not varified</h3>
+             <h3 className='text-sucess'>Please verify your email</h3>
+             <button
+         className='btn btn-primary'
+         onClick={async () => {
+           await sendEmailVerification();
+           alert('Sent email');
+         }}
+       >
+         Send email verification
+       </button>
+         </div>
+       
+     }
+    return children;
+};
+
+export default RequireAuth;
